a52e902357d4d3e00744ed8a470464a4e4a5c05f
黄金贤 <1726283687@qq.com>
2018-10-20 15:02:26 +0800

    字符型字面值的练习代码

---
+++ src/com/imoocProject/CharDemo.java
@@ -0,0 +1,14 @@
+package com.imoocProject;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		// 定义一个变量放字符'a'
+		char a = 'a';
+		System.out.println("a=" + a);
+		char a1 = 65;
+		System.out.println("a1=" + a1);
+
+	}
+
+}
